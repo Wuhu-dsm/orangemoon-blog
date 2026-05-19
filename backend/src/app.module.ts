5325113191f6e159ce5ceb98e2f687586212561d
unknown <737614569@qq.com>
2026-05-19 13:51:33 +0800

    feat(backend): add config module and mongodb connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {
+  databaseConfig,
+  elasticsearchConfig,
+  jwtConfig,
+  redisConfig,
+} from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, redisConfig, elasticsearchConfig, jwtConfig],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow<string>('database.uri'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/config/index.ts
@@ -0,0 +1,4 @@
+export { default as databaseConfig } from './database.config';
+export { default as elasticsearchConfig } from './elasticsearch.config';
+export { default as jwtConfig } from './jwt.config';
+export { default as redisConfig } from './redis.config';
