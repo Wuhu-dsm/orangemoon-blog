68d994715773091c3eca6692161e785a4ceba854
unknown <737614569@qq.com>
2026-05-19 22:01:35 +0800

    feat: wire home dashboard UI

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,11 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Home from '../pages/Home/Home'
-import Layout from '../pages/Layout'
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [{ index: true, element: <Home /> }],
-  },
-])
+export const router = createBrowserRouter([{ path: '/', element: <Home /> }])
